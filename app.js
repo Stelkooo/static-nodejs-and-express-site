66c24798923e8c4f54b835b19b217957230fb147
stelko <stilyanmtoskov@gmail.com>
2022-11-16 14:00:09 +0000

    app.js, project data, templates, static routes

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const { projects } = require("./data/data.json");
+
+app.set("view engine", "pug");
+
+app.use('/static', express.static("./public"));
+
+app.get("/", (req, res) => {
+    res.render('index');
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
+});
+
+app.listen(3000, () => {
+    console.log('The app is running on localhost:3000');
+});
