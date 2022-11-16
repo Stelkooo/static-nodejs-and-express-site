edb8d346e0c6b4cf018445aafdb9c3c473134c9d
stelko <stilyanmtoskov@gmail.com>
2022-11-16 14:03:33 +0000

    project render

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/about", (req, res) => {
     res.render('about');
 });
 
+app.get("/project", (req, res) => {
+    res.render('project');
+});
+
 app.listen(3000, () => {
     console.log('The app is running on localhost:3000');
 });
