d1fffb2f808f4708252fea72c30e1bdab3a49ac0
stelko <stilyanmtoskov@gmail.com>
2022-11-16 14:41:13 +0000

    error handling and passing projects to /project

--- app.js
+++ app.js
@@ -4,20 +4,32 @@ const { projects } = require("./data/data.json");
 
 app.set("view engine", "pug");
 
-app.use('/static', express.static("./public"));
+app.use("/static", express.static("./public"));
 
 app.get("/", (req, res) => {
-    res.render('index');
+    res.render("index", { projects });
 });
 
 app.get("/about", (req, res) => {
-    res.render('about');
+    res.render("about");
 });
 
 app.get("/project", (req, res) => {
-    res.render('project');
+    res.render("project");
+});
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
-    console.log('The app is running on localhost:3000');
+    console.log("The app is running on localhost:3000");
 });
