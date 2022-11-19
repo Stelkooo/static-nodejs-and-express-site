017edbe4d64e76947a4d1c68a7736a1ce723d6e7
stelko <stilyanmtoskov@gmail.com>
2022-11-19 15:07:23 +0000

    printing user friendly error

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    console.log(err.status);
+    console.log("Uh no! Something went wrong!");
     res.status(500).render('error', { err });
 });
 
